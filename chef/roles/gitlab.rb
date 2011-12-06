6ddf2a3c6773620c9b9b1da6a001a87c511c65ea
Daniel McNevin <dan@gobbler.com>
2011-12-05 17:00:57 -0800

    added nginx cookbook

--- chef/roles/gitlab.rb
+++ chef/roles/gitlab.rb
@@ -3,10 +3,12 @@ description "GitLab Web App"
 
 run_list(
   "recipe[base_packages]",
+  "recipe[nginx]",
   "recipe[rvm::system]",
   "recipe[rvm::vagrant]",
   "recipe[vagrant]",
   "recipe[pygments]",
   "recipe[gitosis]",
-  "recipe[gitlab]"
+  "recipe[gitlab]",
+  "recipe[gitlab::nginx]"
 )
